ced9b4f42cc3af39af9e799b0cef2dfb71cd67d0
heldbrendel <mariuswagner@gmail.com>
2024-02-29 20:12:04 +0100

    remove imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,5 @@
 <script>
-	import "../app.css";
-	import {
-		Button,
-		Input,
-		Label,
-		Modal,
-		Table,
-		TableBody,
-		TableBodyCell,
-		TableBodyRow,
-		TableHead, TableHeadCell
-	} from 'flowbite-svelte';
+	import '../app.css';
 </script>
 
 <nav class="fixed w-full bg-white border-b border-gray-200 z-30">
@@ -27,16 +16,21 @@
 					<ul class="space-y-2 pb-2">
 						<li>
 							<a href="/" class="text-base text-gray-900 font-normal flex items-center p-2 hover:bg-gray-100 group">
-								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-									<path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+										 stroke="currentColor" class="w-6 h-6">
+									<path stroke-linecap="round" stroke-linejoin="round"
+												d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
 								</svg>
 								<span class="ml-3 flex-1 whitespace-nowrap">Dashboard</span>
 							</a>
 						</li>
 						<li>
-							<a href="/settings" class="text-base text-gray-900 font-normal flex items-center p-2 hover:bg-gray-100 group">
-								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-									<path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
+							<a href="/settings"
+								 class="text-base text-gray-900 font-normal flex items-center p-2 hover:bg-gray-100 group">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+										 stroke="currentColor" class="w-6 h-6">
+									<path stroke-linecap="round" stroke-linejoin="round"
+												d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
 								</svg>
 								<span class="ml-3 flex-1 whitespace-nowrap">Settings</span>
 							</a>
