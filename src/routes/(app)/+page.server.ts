79e26809632e2a4a02937a67e5f7dd092f4c420d
heldbrendel <mariuswagner@gmail.com>
2024-03-13 20:38:46 +0100

    add todo and remove prisma client

--- src/routes/(app)/+page.server.ts
+++ src/routes/(app)/+page.server.ts
@@ -6,5 +6,6 @@ export const load: PageServerLoad = async ({ locals }) => {
 	if (locals.auth === undefined) {
 		error(401, 'unauthorized');
 	}
+	// TODO retrieve from api and use cache
 	return { animes: (await getUserAnimeList(locals.auth.access_token)).sort((a, b) => b.id - a.id) };
 };
